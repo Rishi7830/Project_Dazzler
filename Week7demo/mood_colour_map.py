40138ef95dd0f35898fc7ff31311e79715ed739f
1Annanya3 <133660642+1Annanya3@users.noreply.github.com>
2025-09-30 15:55:08 +0800

    Create mood_colour_map.py

---
+++ Week7demo/mood_colour_map.py
@@ -0,0 +1,10 @@
+mood_color_map = {
+    "Pleasure": (255, 215, 0),     # Gold Yellow
+    "Excitement": (255, 69, 0),    # Orange Red
+    "Arousal": (255, 0, 0),        # Red
+    "Distress": (139, 0, 0),       # Dark Red
+    "Displeasure": (128, 0, 128),  # Purple
+    "Depression": (0, 0, 139),     # Dark Blue
+    "Sleepiness": (0, 0, 255),     # Blue
+    "Relaxation": (135, 206, 250)  # Light Sky Blue
+}
