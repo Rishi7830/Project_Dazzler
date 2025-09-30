c88d7c4ff14c53e7510d46111a708beed2039fa6
1Annanya3 <133660642+1Annanya3@users.noreply.github.com>
2025-09-30 15:58:53 +0800

    Update Buffer_Manager_Week7.py

--- Week7demo/Buffer_Manager_Week7.py
+++ Week7demo/Buffer_Manager_Week7.py
@@ -6,11 +6,9 @@ class AudioBuffer:
         self.buffer = np.zeros(window_size, dtype=np.float32)
 
     def update(self, new_chunk):
-        """Update buffer with new audio chunk (length = HOP_SIZE)."""
         chunk_len = len(new_chunk)
         self.buffer = np.roll(self.buffer, -chunk_len)
         self.buffer[-chunk_len:] = new_chunk
 
     def get_window(self):
-        """Return current window of audio (length = WINDOW_SIZE)."""
         return self.buffer.copy()
