3047f762924f6e8fce581543049f61129df29033
Rishi <141700726+Rishi7830@users.noreply.github.com>
2025-10-07 10:56:24 +0800

    Update main_win.py
    com0

--- Week5demo/main_win.py
+++ Week5demo/main_win.py
@@ -25,7 +25,7 @@ except Exception:
 def _suggest_default_port() -> str:
     sysname = platform.system().lower()
     if sysname.startswith("win"):
-        return os.environ.get("DAZZLER_DMX_PORT", "COM4")
+        return os.environ.get("DAZZLER_DMX_PORT", "/dev/ttyUSB0")
     if sysname == "darwin":
         return os.environ.get("DAZZLER_DMX_PORT", "/dev/tty.usbserial")
     return os.environ.get("DAZZLER_DMX_PORT", "/dev/ttyUSB0")
