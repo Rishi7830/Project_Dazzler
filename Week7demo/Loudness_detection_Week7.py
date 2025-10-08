6af0594bb88262b1725af39979a22d2716d41897
1Annanya3 <133660642+1Annanya3@users.noreply.github.com>
2025-10-08 22:24:10 +0800

    Update Loudness_detection_Week7.py
    ttrying to debug

--- Week7demo/Loudness_detection_Week7.py
+++ Week7demo/Loudness_detection_Week7.py
@@ -1,6 +1,8 @@
 import essentia.standard as es
+import numpy as np
 
 def detect_loudness(audio_buffer):
+    print("Audio buffer min/max:", np.min(audio_buffer), np.max(audio_buffer)) #for debugging
     """
     Returns loudness in dB from an audio buffer.
     """
@@ -9,4 +11,10 @@ def detect_loudness(audio_buffer):
         loudness = loudness_extractor(audio_buffer)
     except Exception:
         loudness = -60.0  # Quiet fallback
+
+    print("Raw loudness:", loudness)
+    if np.isnan(loudness) or loudness > 0 or loudness < -80:
+        print("Anomalous loudness detected:", loudness)
+        loudness = -30
+
     return loudness
