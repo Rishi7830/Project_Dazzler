4aa794cd820a2d9c1f1bd5ba9f3775e62b0e24d2
Rishi <141700726+Rishi7830@users.noreply.github.com>
2025-10-12 12:04:34 +0800

    Update pyserial.py

--- Week7demo/pyserial.py
+++ Week7demo/pyserial.py
@@ -166,7 +166,7 @@ class SimpleDMX:
             print(f"Serial port {self.port} closed.")
 
 if __name__ == "__main__":
-    SERIAL_PORT = "/dev/ttyUSB1"
+    SERIAL_PORT = "/dev/ttyUSB1" #Changed from USB0
     dmx = SimpleDMX(port=SERIAL_PORT, strobe_interval=0.5)
 
     if not dmx.ser:
@@ -242,4 +242,5 @@ if __name__ == "__main__":
             print("\nExiting DMX test...")
         finally:
             dmx.close()
+
 
